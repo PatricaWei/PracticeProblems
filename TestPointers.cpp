bff34063195a3e986f88d41a854a7d3370cb86bc
Patricia Wei <patricia.wei.2201@gmail.com>
2017-03-29 10:44:10 -0700

    Create TestPointers.cpp

---
+++ TestPointers.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+void assignPointers()
+{
+   int *p1, *p2;
+   p1 = new int;
+   p2 = new int;
+
+   *p1 = 10;
+   *p2 = 20;
+
+   cout << *p1 << " " << *p2 << endl;
+   p1 = p2;
+   cout << *p1 << " " << *p2 << endl;
+   *p1 = 30;
+   cout << *p1 << " " << *p2 << endl;
+}
+/*------------ RUN -----------------
+ 10 20
+ 20 20
+ 30 30
+ -----------------------------------*/
+
+void assignVariables()
+{
+   int *p1, *p2;
+   p1 = new int;
+   p2 = new int;
+
+   *p1 = 10;
+   *p2 = 20;
+
+   cout << *p1 << " " << *p2 << endl;
+   *p1 = *p2;
+   cout << *p1 << " " << *p2 << endl;
+   *p1 = 30;
+   cout << *p1 << " " << *p2 << endl;
+}
+/*------------ RUN -----------------
+10 20
+20 20
+30 20
+ -----------------------------------*/
+
+
+int main()
+{
+   assignPointers();
+   cout << endl;
+   assignVariables();
+}
